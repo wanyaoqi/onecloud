107d7f6c925d6c1d8a7ea5f49c997957b4d0792c
wanyaoqi <wanyaoqi@yunionyun.com>
2020-08-14 13:08:34 +0800

    patch host bridge

--- pkg/util/netutils2/netutils_linux.go
+++ pkg/util/netutils2/netutils_linux.go
@@ -44,15 +44,38 @@ func (n *SNetInterface) GetAddresses() [][]string {
 	return r
 }
 
-func (n *SNetInterface) GetRoutes(gwOnly bool) [][]string {
-	rs, err := iproute2.NewRoute(n.name).List4()
+func (n *SNetInterface) GetGatewayRoutes() [][]string {
+	return n.getRoutes(true)
+}
+
+func (n *SNetInterface) GetRouteSpecs() []iproute2.RouteSpec {
+	routespecs, err := iproute2.NewRoute(n.name).List4()
 	if err != nil {
 		return nil
 	}
+	return routespecs
+}
 
-	res := [][]string{}
-	for i := range rs {
-		r := &rs[i]
+func (n *SNetInterface) GetGatewayRouteSpecs() []iproute2.RouteSpec {
+	routespecs, err := iproute2.NewRoute(n.name).List4()
+	if err != nil {
+		return nil
+	}
+	for j := len(routespecs) - 1; j >= 0; j-- {
+		routespec := &routespecs[j]
+		if masklen, _ := routespec.Dst.Mask.Size(); masklen != 0 {
+			routespecs = append(routespecs[:j], routespecs[j+1:]...)
+		}
+	}
+	return routespecs
+}
+
+func (n *SNetInterface) getRoutes(gwOnly bool) [][]string {
+	routespecs := n.GetRouteSpecs()
+
+	var res [][]string
+	for i := range routespecs {
+		r := &routespecs[i]
 		ok := true
 		if masklen, _ := r.Dst.Mask.Size(); gwOnly && masklen != 0 {
 			ok = false
